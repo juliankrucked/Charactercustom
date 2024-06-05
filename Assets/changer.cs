3dbc7b78eb938ceb8015b2429e141c41fe24028c
lauris <solaimans1203@gmail.com>
2024-06-05 21:02:38 +0300

    spēlētāja izskata maiņa

---
+++ Assets/changer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outfitchanger : MonoBehaviour
+{
+    [Header("Sprite To Change")]
+    public SpriteRenderer bodyPart;
+
+    [Header("Sprite To Cycle Thtough")]
+    public List<Sprite> options = new List<Sprite>();
+
+    private int currentOption = 0; 
+
+    public void NextOption()
+    {
+        currentOption++;
+        if(currentOption >= options.Count)
+        {
+            currentOption = 0;
+        }
+        bodyPart.sprite = options[currentOption];
+    }
+
+    public void  PreviousOption()
+    {
+        currentOption--;
+        if(currentOption <= 0)
+        {
+            currentOption = options.Count - 1;
+        }
+        bodyPart.sprite = options[currentOption];
+    }
+}
