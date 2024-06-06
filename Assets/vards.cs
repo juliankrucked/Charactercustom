71aba3f7aa79eb0ab9c1e820bc284a595071c133
lauris <solaimans1203@gmail.com>
2024-06-06 21:13:32 +0300

    izveidots inputfield un text

---
+++ Assets/vards.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class vards : MonoBehaviour {
+    
+    public string teksts;
+    public GameObject name_text;
+    public GameObject exitingname;
+
+    public void uzglabaTekstu(){
+        teksts = name_text.GetComponent<Text>().text;
+        exitingname.GetComponent<Text>().text = "Hi there, they call me "+ teksts.ToUpper() +" and I am Rouge!";
+    }
+}
