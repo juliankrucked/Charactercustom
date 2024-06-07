642701a1d675ae9c87ab480319800318f79563ed
lauris <solaimans1203@gmail.com>
2024-06-07 12:33:18 +0300

    izdevās izvadīt dzimšanas gadu

--- Assets/dropdown.cs
+++ Assets/dropdown.cs
@@ -1,64 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class dropdown : MonoBehaviour
+public class DropdownController : MonoBehaviour
 {
-    public Dropdown Dropdown;
-
-    public GameObject Rogue_01;
-    public GameObject Rogue_02;
-    public GameObject Rogue_03;
-    public GameObject Rogue_04;
-    public GameObject Rogue_05;
-    public GameObject Rogue_06;
-
-    private GameObject selectedPrefab;
-
-    public void change()
-    {
-        // Initialize the dropdown with options
-        Dropdown.options.Clear();
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_01" });
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_02" });
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_03" });
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_04" });
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_05" });
-        Dropdown.options.Add(new Dropdown.OptionData() { text = "Rogue_06" });
-
-        Dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(Dropdown); });
-
-
-        selectedPrefab = Rogue_01;
-    }
-
-    public void DropdownItemSelected(Dropdown dropdown)
-    {
-        int index = dropdown.value;
-
-        switch (index)
-        {
-            case 0:
-                selectedPrefab = Rogue_01;
-                break;
-            case 1:
-                selectedPrefab = Rogue_02;
-                break;
-            case 2:
-                selectedPrefab = Rogue_03;
-                break;
-            case 3:
-                selectedPrefab = Rogue_04;
-                break;
-            case 4:
-                selectedPrefab = Rogue_05;
-                break;
-            case 5:
-                selectedPrefab = Rogue_06;
-                break;
-        }
-         selectedPrefab.transform.localScale = new Vector3(selectedPrefab.transform.localScale.z);
-        
-    }
-
+    public Dropdown dropdown;
+    public GameObject[] prefabs; 
+    private GameObject currentPrefabInstance; 
     
+    void Start()
+    {
+        dropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        foreach (GameObject prefab in prefabs)
+        {
+            options.Add(prefab.name);
+        }
+
+        dropdown.AddOptions(options);
+
+        dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropdown);
+        });
+    }
+
+    void DropdownValueChanged(Dropdown dropdown)
+    {
+        int selectedIndex = dropdown.value;
+
+        if (selectedIndex >= 0 && selectedIndex < prefabs.Length)
+        {
+            if (currentPrefabInstance != null)
+            {
+                Destroy(currentPrefabInstance);
+            }
+
+            currentPrefabInstance = Instantiate(prefabs[selectedIndex], Vector3.zero, Quaternion.identity);
+        }
+    }
 }
--- Assets/vards.cs
+++ Assets/vards.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class vards : MonoBehaviour {
     
     public string teksts;
+    public int birth;
+    public TMP_InputField birthyear;
     public GameObject name_text;
     public GameObject exitingname;
 
-    public void uzglabaTekstu(){
-        teksts = name_text.GetComponent<Text>().text;
-        exitingname.GetComponent<Text>().text = "Hi there, they call me "+ teksts.ToUpper() +" and I am Rogue!";
+    public void savedata() {
+        if (name_text != null && birthyear != null && exitingname != null) {
+            teksts = name_text.GetComponent<Text>().text;
+            string birthyearString = birthyear.text;
+
+            birthyearString = birthyearString.Trim();
+
+            if (int.TryParse(birthyearString, out birth)) {
+                int currentYear = DateTime.Now.Year;
+                int age = currentYear - birth;
+
+                exitingname.GetComponent<Text>().text = "Hi there, they call me " + teksts.ToUpper() + " and I am Rouge! I was born in " + birth + ". I am " + age + " years old.";
+            } else {
+                Debug.LogError("The birthyear text is not a valid integer.");
+                exitingname.GetComponent<Text>().text = "Invalid birth year!";
+            }
+        } else {
+            Debug.LogError("Please assign the name_text, birthyear, and exitingname GameObjects in the Inspector.");
+        }
     }
 }
